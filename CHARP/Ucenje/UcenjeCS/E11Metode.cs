1a157a17db1b0ece0ac858ca6c353620d652e170
itomicbabinski <168195864+itomicbabinski@users.noreply.github.com>
2024-06-27 18:55:24 +0200

    VinylBook

---
+++ CHARP/Ucenje/UcenjeCS/E11Metode.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UcenjeCS
+{
+    internal class E11Metode
+    {
+        public static void Izvedi()
+        {
+            for (int i = 0; i < 55; i++)
+            {
+                Tip1();
+            }
+        }
+
+        private static void Tip1()
+        {
+            Console.WriteLine("Prvi red");
+        }
+
+    }
+}
---
+++ CHARP/Ucenje/UcenjeCS/E13KlasaObjekt/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UcenjeCS.E13KlasaObjekt
+{
+    public class Program
+    {
+        public static void Izvedi()
+        {
+            Osoba osoba = new Osoba();
+            osoba.Ime = "Pero";
+            osoba.Prezime = "Perić";
+            Console.WriteLine(osoba.ImePrezime());
+            Osoba ravnatelj = new Osoba();
+            osoba.Ime = "Eduard";
+            osoba.Prezime = "Kuzma";
+
+            var direktor = new Osoba { Prezime = "Kas" };
+            Console.WriteLine(direktor.Prezime?.ToUpper());
+
+
+
+        }
+
+    }
+}
