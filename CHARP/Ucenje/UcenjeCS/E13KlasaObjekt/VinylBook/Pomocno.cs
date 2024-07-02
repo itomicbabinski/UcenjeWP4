5673a402c26bc92bce1c289e22c41910e6806f74
itomicbabinski <i.tomic.babinski@gmail.com>
2024-07-02 16:37:02 +0200

    KlasaObjekt1

---
+++ CHARP/Ucenje/UcenjeCS/E13KlasaObjekt/VinylBook/Pomocno.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UcenjeCS.E13KlasaObjekt.VinylBook
+{
+    public static int InsertUser(int odBroja, int doBroja)
+    {
+        int suma = 0;
+        for (int i = odBroja; i <= doBroja; i++)
+        {
+            suma += i;
+        }
+
+        return suma;
+        //throw new NotImplementedException(); može i ovako
+    }
+
+
+
+    public static int InsertString(string stname, )
+    {
+        int suma = 0;
+        for (int i = odBroja; i <= doBroja; i++)
+        {
+            suma += i;
+        }
+
+        return suma;
+        //throw new NotImplementedException(); može i ovako
+    }
+}
